73ac5da25762bc09d7e414e50432628afd9e223b
OBMULTISERVICES <multiservice.lopez@gmail.com>
2026-04-05 22:31:09 -0700

    Implementación inicial de calculo de cortante y notas MIT

---
+++ cortante_viga.py
@@ -0,0 +1,6 @@
+reaccion_pared_y = 150
+fuerza_cortante = -reaccion_pared_y
+print("La fuerza cortante interna del acero es:")
+print(fuerza_cortante)
+# Repasando la leccion 1 del MIT : INTRODUCCION A LA COMPUTACION
+# Este codigo es la base en mi portafolio en OSSU
